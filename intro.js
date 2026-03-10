6edbf81ee804c2d40275e24ac58a7bfc1ba77f0e
olalekan <eritofunmiolamilekan@gmail.com>
2026-03-10 23:42:09 +0100

    first commit

---
+++ intro.js
@@ -0,0 +1,5 @@
+const myName = "Akindeyinde Olalekan Samuel"
+const myAge = 21
+const favProgLang = "Javascript"
+
+console.log(`My name is ${myName}, I'm ${myAge} years old and my favorite programing language is ${favProgLang}`)
