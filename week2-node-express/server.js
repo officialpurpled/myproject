e80b802444f1c4c1e8ac6148cc127bd072f82dea
olalekan <officialpurpledragon01@gmail.com>
2026-03-28 15:11:07 +0100

    minor adjust

--- week2-node-express/server.js
+++ week2-node-express/server.js
@@ -8,37 +8,34 @@ const app = express()
 const PORT = process.env.PORT
 
 //Middlewares
-function logger(req, res, next){
-  console.log(`Your request method is ${req.method}, with URL: ${req.url}`)
-
-  next()
-}
-
 app.use(express.json())
 app.use(cors())
 
-app.use(logger)
+function logger(req, res, next){
+  console.log(`Your request method is ${req.method}, with URL: ${req.url}`)
+  next()
+}
 
 //Roures
-app.get('/', (req, res) =>{
+app.get('/', logger,(req, res) =>{
   res.status(200).send('My Week 2 API!')
 })
 
-app.get('/user/:id', (req, res)=>{
-  const userId = req.param.id
-  res.status(200).send(`User [${userId}] profile`)
-})
-
-app.post('/user', (req, res)=> {
+app.post('/user', logger, (req, res)=> {
   const {name, email} = req.body
-
+  
   if (!name || !email){
-    res.status(400).send('Empty field detected. All fields are required')
+    return res.status(400).send('Empty field detected. All fields are required')
   }
   
   res.status(200).send(`Hello, ${name}`)
 })
 
+app.get('/user/:id', (req, res)=>{
+  const userId = req.params.id;
+
+  res.status(200).send(`User [${userId}] profile.`)
+})
 
 //init server
 app.listen(PORT, ()=>{
